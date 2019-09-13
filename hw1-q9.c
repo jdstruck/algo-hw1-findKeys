bfe30f6e52d4f8006e95370c7fed58c5ca028864
Jesse Struck <jdstruck@gmail.com>
2019-09-13 06:13:41 -0400

    update

--- hw1-q9.c
+++ hw1-q9.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
         int A[] = {0,1,0,1,0,1,0,1};
         int size = sizeof(A)/sizeof(A[0]);
@@ -9,13 +9,27 @@ void main()
                 printf("%d", A[i]); 
         }
 
-        for (int i=1; i<size; ++i) {
-                int prev = A[0];
-                if (A[i] > prev) {
-                        swap()
+        printf("\n\n");
+
+        for (int i=0; i<size/2; ++i) {
+                int prev = A[i];
+                for (int j=i+1; j<size-i; ++j) {
+                        if (A[j] > prev) {
+                                int tmp = A[j];
+                                A[j] = prev;
+                                A[j-1] = tmp;
+                                for (int i=0; i<size; ++i) {
+                                        printf("%d", A[i]); 
+                                }
+                                printf("\n"); 
+                        }
+                        prev = A[j];
                 }
-                printf("%d", A[i]); 
         }
 
-        
+        //for (int i=0; i<size; ++i) {
+        //        printf("%d", A[i]); 
+        //}
+
+        return 0;        
 }
--- hw1.cpp
+++ hw1.cpp
@@ -3,18 +3,18 @@
 #include <vector>
 
 void findKey(std::vector<int> A) {
-
+        std::sort(A.begin(), A.end());
         for (int i=0; i < A.size(); ++i) {
                 std::cout << A[i] << std::endl;
-                for (int j=0; j < A.size(); ++j) {
-                        std::cout << A[j] << std::endl;
-                }
+                //for (int j=0; j < A.size(); ++j) {
+                //        std::cout << A[j] << std::endl;
+                //}
         }
 }
 
 int main() {
         auto t1 = std::chrono::high_resolution_clock::now();
-        std::vector<int> A = {18,23,4,35,99,67,198,20,38,55,2,18,19,487,11,40,10,3,27,22};
+        std::vector<int> A = {18,23,4,35,99,67,198,20,38,55,2,18,19,487,11,40,10,13,27,22};
         findKey(A);
         auto t2 = std::chrono::high_resolution_clock::now();
 
