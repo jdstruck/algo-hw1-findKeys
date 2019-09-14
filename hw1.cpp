ba8266bb22af10c0f95fb494cfabad61767d0f13
Jesse Struck <jdstruck@gmail.com>
2019-09-14 12:20:12 -0400

    working algo, TODO file handling

--- hw1.cpp
+++ hw1.cpp
@@ -1,21 +1,42 @@
 #include <iostream>
 #include <chrono>
 #include <vector>
+#include <unordered_set>
+#include <functional>
 
-void findKey(std::vector<int> A) {
-        std::sort(A.begin(), A.end());
-        for (int i=0; i < A.size(); ++i) {
-                std::cout << A[i] << std::endl;
-                //for (int j=0; j < A.size(); ++j) {
-                //        std::cout << A[j] << std::endl;
-                //}
+void findSumKeys(std::vector<int> A, int key) {
+        std::unordered_set<int> uset;
+        for (int i=0; i<A.size(); ++i) { 
+                int diff = key-A[i];
+                if (diff < 0) continue;
+                if (uset.find(diff) == uset.end()) {
+                        uset.emplace(A[i]);
+                } else {
+                        std::cout << key << ": ";
+                        std::cout << key-A[i] << " ";
+                        std::cout << A[i] << std::endl;
+                        uset.erase(A[i]);
+                }                      
         }
 }
 
 int main() {
         auto t1 = std::chrono::high_resolution_clock::now();
         std::vector<int> A = {18,23,4,35,99,67,198,20,38,55,2,18,19,487,11,40,10,13,27,22};
-        findKey(A);
+        std::sort(A.begin(), A.end());//, std::greater<int>());
+
+        //for (auto key: A) {
+        //for (int i=0; i<A.size(); ++i) { 
+        for (int i=A.size()-1; i >= 0; --i) { 
+                //std::cout << A[i] << " ";
+                int key = A[i];
+
+                findSumKeys(A,key);
+
+                A.erase(A.end()-1);
+        
+        }
+
         auto t2 = std::chrono::high_resolution_clock::now();
 
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count();
